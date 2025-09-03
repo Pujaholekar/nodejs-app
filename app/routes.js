5a3767ae63e9af08391732d7420e9d2b7cc4af14
Pujaholekar <pujaholekar@gmail.com>
2025-09-03 12:13:13 +0530

    Update routes.js

--- app/routes.js
+++ app/routes.js
@@ -2,4 +2,8 @@ module.exports = function(app) {
     app.get('/', function(req, res){
         res.render('index'); // index view file is rendered when HTTP GET '/' called
     });
+    app.get('/health', (req, res) => {
+  res.sendStatus(200);
+});
+
 }
